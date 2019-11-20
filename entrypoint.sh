125bfc60a645f6835a89db25610963359ce2545a
Jamie Johnstone <j@miej.co>
2019-11-20 11:02:15 +0000

    Added conditional test for cache bust

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,6 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   exit 1
 fi
 
-if [ -z "$AWS_CF_ID" ]; then
-  echo "AWS_CF_ID is not set. Quitting."
-  exit 1
-fi
-
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
@@ -58,8 +53,14 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
-              
-sh -c "aws cloudfront create-invalidation --distribution-id ${AWS_CF_ID} --paths \"/*\""
+
+# Check if Cloudfront Cache ID is set - if so, create an invalidation.
+
+if [ "$AWS_CF_ID" ]; then
+  sh -c "aws cloudfront create-invalidation --distribution-id ${AWS_CF_ID} --paths \"/*\""
+else
+  echo "AWS_CF_ID is not set. Skipping cache bust step..."
+fi
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
